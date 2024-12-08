f3b90fe66d8830db9bbd3307377270edc7aa4043
codingbox <jhonericssonytac365@gmail.com>
2024-12-08 20:09:40 +0800

    Add table creation and deletion functionality in the main window

--- dialogs.py
+++ dialogs.py
@@ -91,3 +91,130 @@ class Register(QDialog):
     def closeEvent(self, event):
         self.reject()
         self.close()
+
+class CreateTable(QDialog):
+    
+    def __init__(self, user):
+        super(CreateTable, self).__init__()
+        uic.loadUi('ui/crearetable.ui', self)
+        
+        self.__user = user
+
+        self.createbtn.clicked.connect(self.create)
+        self.cancelbtn.clicked.connect(self.cancel)
+        self.addbtn.clicked.connect(self.add)
+        self.minusbtn.clicked.connect(self.minus)
+    
+    def create(self):
+        conn = sqlite3.connect('databases/{}.db'.format(self.__user.get_username()))
+        cursor = conn.cursor()
+
+        table_name = self.tableName.text()
+
+        # Check if table name is empty
+        if table_name == '':
+            QMessageBox.warning(self, 'Error', 'Please specify a table name')
+            return
+        
+        # Check if table name is valid
+        cursor.execute('SELECT name FROM sqlite_master WHERE type = "table" AND name = ?', (table_name,))
+        if cursor.fetchone() is not None:
+            QMessageBox.warning(self, 'Error', 'Table already exists')
+            return
+
+        # Check if there are no columns
+        if self.table.rowCount() == 0:
+            QMessageBox.warning(self, 'Error', 'Please add columns')
+            return
+
+        # Check if there are empty column names
+        for i in range(self.table.rowCount()):
+            if self.table.item(i, 0) is None:
+                QMessageBox.warning(self, 'Error', 'Please fill all column names')
+                return
+        
+        # Check if there are empty column types
+        for i in range(self.table.rowCount()):
+            if self.table.item(i, 1) is None:
+                QMessageBox.warning(self, 'Error', 'Please fill all column types')
+                return
+        
+        # Check if there are duplicate column names
+        column_names = []
+        for i in range(self.table.rowCount()):
+            if self.table.item(i, 0).text() in column_names:
+                QMessageBox.warning(self, 'Error', 'Duplicate column names')
+                return
+            column_names.append(self.table.item(i, 0).text())
+        
+        # Check if there is only one primary key
+        primary_keys = 0
+        for i in range(self.table.rowCount()):
+            
+            if self.table.item(i, 2) is None:
+                continue
+
+            if self.table.item(i, 2).text() == 'P' or self.table.item(i, 2).text() == 'p':
+                primary_keys += 1
+            if primary_keys > 1:
+                QMessageBox.warning(self, 'Error', 'Only one primary key is allowed')
+                return
+
+        # Check if there are no primary keys
+        if primary_keys == 0:
+            QMessageBox.warning(self, 'Error', 'Please specify a primary key')
+            return
+        
+        # Create table
+        query = """
+        CREATE TABLE {} (
+        """.format(table_name)
+        foriegn_key = []
+
+        for i in range(self.table.rowCount()):
+            name_ = self.table.item(i, 0).text()
+            type_ = self.table.item(i, 1).text()
+            keyType = self.table.item(i, 2).text() if self.table.item(i, 2) is not None else ''
+            
+            if keyType == 'P' or keyType == 'p':
+                keyType = 'PRIMARY KEY'
+            elif ',' in keyType:
+                
+                if keyType.count(',') > 1:
+                    QMessageBox.warning(self, 'Error', 'Foreign key input error')
+                    return
+                
+                keyType = keyType.replace(' ', '')
+                
+                foriegn_key = keyType.split(',')
+                foriegn_key.append(name_)
+
+            query += '{} {} {},\n'.format(name_, type_, keyType)
+        
+        if foriegn_key:
+            query += 'FOREIGN KEY ({}) REFERENCES {}({}),\n'.format(
+                foriegn_key[2], foriegn_key[0], foriegn_key[1]
+            )
+        
+        query = query[:-2] + ');'
+
+        cursor.execute(query)
+        conn.commit()
+        conn.close()
+
+        self.accept()
+        self.close()
+    
+    def cancel(self):
+        self.reject()
+        self.close()
+    
+    def closeEvent(self, event):
+        self.reject()
+        self.close()
+    
+    def add(self):
+        self.table.insertRow(self.table.rowCount())
+    
+    def minus(self):
+        self.table.removeRow(self.table.rowCount() - 1)
--- mainwindow.py
+++ mainwindow.py
@@ -1,8 +1,9 @@
-from PyQt6.QtWidgets import QMainWindow
+from PyQt6.QtWidgets import QMainWindow, QMessageBox
 from PyQt6 import uic
 from PyQt6.QtCore import Qt
 from User import User
-from Dialogs import Login, Register
+from Dialogs import Login, Register, CreateTable
+import sqlite3
 
 class MainWindow(QMainWindow):
     def __init__(self):
@@ -11,14 +12,13 @@ class MainWindow(QMainWindow):
 
         self.setFixedSize(self.size())
 
-
         self.__user: User = User()
 
         while True:
             login: Login = Login()
             login.exec()
 
-            print(login.result())
+            print("Logged in" if login.result() == 1 else "Registering") # Debugging
 
             if login.result() == 1:
                 self.__user.set_username(login.username.text())
@@ -27,9 +27,56 @@ class MainWindow(QMainWindow):
             else:
                 register = Register()
                 register.exec()
-                
         
-        self.show()
-        
+        self.createTableBtn.clicked.connect(self.create_table)
+        self.deleteTableBtn.clicked.connect(self.delete_table)
 
+        self.diplayTables()
+
+        self.show()
+
+    def delete_table(self):
+        table = self.listWidget.currentItem().text() if self.listWidget.currentItem() is not None else ''
+
+        if table == '':
+            QMessageBox.warning(self, 'Error', 'Please select a table to delete')
+            return
+        
+        response = QMessageBox.question(self, 'Delete Table', 'Are you sure you want to delete the table?', QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No)
+
+        if response is not QMessageBox.StandardButton.Yes:
+            return
+
+        conn = sqlite3.connect('databases/{}.db'.format(self.__user.get_username()))
+        cursor = conn.cursor()
+
+        cursor.execute("DROP TABLE {}".format(table))
+        conn.commit()
+
+        conn.close()
+
+        self.listWidget.takeItem(self.listWidget.currentRow())
+        self.terminal.append(f"Table {table} is deleted")
+
+    def create_table(self):
+
+        dialog = CreateTable(self.__user)
+        dialog.exec()
+
+        if dialog.result() == 1:
+            self.terminal.append("Table created successfully")
+            self.listWidget.addItem(dialog.tableName.text())
+        
+    def diplayTables(self):
+        conn = sqlite3.connect('databases/{}.db'.format(self.__user.get_username()))
+        cursor = conn.cursor()
+
+        cursor.execute("SELECT name FROM sqlite_master WHERE type='table';")
+        tables = cursor.fetchall()
+
+        for table in tables:
+            self.listWidget.addItem(table[0])
+            print(table[0])
+
+        conn.close()
         
